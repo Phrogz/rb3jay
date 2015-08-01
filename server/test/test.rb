d0225ba303a8b75a4eb0463de4f55d284629e3d4
Gavin Kistner <gavin@phrogz.net>
2015-07-31 23:29:35 -0600

    Fix unit test framework to start/stop process correctly, and wait for socket connection

--- server/test/test.rb
+++ server/test/test.rb
@@ -1,42 +1,36 @@
 require 'socket'
 require 'json'
-require 'minitest/unit'
+require 'minitest/autorun'
 
-PORT = 7336
+PORT = 7331
 CMD = File.expand_path("../../bin/rb3jay",__FILE__)
 
 class TestServer < MiniTest::Unit::TestCase
-  def setup
-    @thread = Thread.new{ `#{CMD} -D --port #{PORT}` }
-    sleep 0.5
-    @socket = TCPSocket.open('localhost', PORT)
-    STDOUT.puts "hi mom"
-  end
+	def setup
+		@pid = Process.spawn "#{CMD} -D --port #{PORT}"
+		begin
+			@socket = TCPSocket.open('localhost', PORT)
+		rescue Errno::ECONNREFUSED
+			sleep 0.1
+			retry
+		end
+	end
 
-  def teardown
-  	@socket.close if @socket
-  	@thread.kill  if @thread
-  end
+	def teardown
+		if @socket && !@socket.closed?
+			@socket.puts({cmd:"quit"}.to_json)
+			@socket.close
+		end
+		Process.kill( 'HUP', @pid )
+	end
 
-  def send(data)
-  	@socket.print(data.to_json)
-  	JSON.parse @socket.gets.chomp
-  end
+	def tell(data)
+		@socket.puts(data.to_json)
+		JSON.parse @socket.gets.chomp
+	end
 
-  def test_bad_commands
-    refute_nil @thread
-    refute_nil @socket
-    assert_false send(false)['ok'],    "Missing command should cleanly fail."
-    assert_false send(cmd:"no")['ok'], "Unrecognized command should cleanly fail."
-  end
+	def test_bad_commands
+		refute tell(false)['ok'],    "Missing command should cleanly fail."
+		refute tell(cmd:"no")['ok'], "Unrecognized command should cleanly fail."
+	end
 end
-
-runner = MiniTest::Unit.new
-t = TestServer.new( runner )
-p :setup
-t.setup
-p :testbad
-t.test_bad_commands
-p :teardown
-t.teardown
-
