75e9d382793ee832593e95de98c82c791a537a8a
Gavin Kistner <gavin@phrogz.net>
2015-08-04 08:45:18 -0600

    Create file-based DB when given a directory

--- server/lib/models/init.rb
+++ server/lib/models/init.rb
@@ -1,7 +1,7 @@
 require 'sequel'
 
 Sequel.extension :migration
-ARGS[:db] = ARGS[:nodb] ? Sequel.sqlite : Sequel.sqlite
+ARGS[:db] = ARGS[:nodb] ? Sequel.sqlite : Sequel.sqlite(File.join(ARGS[:directory],'rb3jay.db'))
 Sequel::Migrator.run ARGS[:db], File.expand_path('../migrations',__FILE__)
 
 require_relative 'song'
