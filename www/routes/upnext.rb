d32a1c79cdb0403d641747e3018efbefeb9ed9c0
Gavin Kistner <gavin@phrogz.net>
2016-02-04 16:10:38 -0700

    Set song priority for user songs

--- www/routes/upnext.rb
+++ www/routes/upnext.rb
@@ -24,6 +24,7 @@ class RB3Jay < Sinatra::Application
 	def add_user_queues
 		start_time = lap_time = Time.now
 		songs_in_list = Set.new @mpd.queue.map(&:file)
+		initial_song_count = songs_in_list.length
 		puts "%-20s: %.3fs" % ["songs_in_list",(t=Time.now)-lap_time]; lap_time=t
 		sql = "SELECT user,sum(duration) FROM song_events WHERE user NOT NULL AND event='play' AND strftime('%Y-%m-%d',`when`)=:today GROUP BY user"
 		played_by_user = Hash[ @db[sql,today:Date.today.strftime('%Y-%m-%d')].map(&:values) ]
@@ -43,9 +44,6 @@ class RB3Jay < Sinatra::Application
 			if (queue=queues_by_user[lowest_user]) && (song=queue.shift)
 				unless songs_in_list.include?(song.file)
 					files_and_users << [song.file,lowest_user]
-
-					# @mpd.song_priority(2,{id:[song_id]})
-					# puts "%-20s: %.3fs" % ["set priority",(t=Time.now)-lap_time]; lap_time=t
 					played_by_user[lowest_user] += song.track_length
 					songs_in_list << song.file
 				end
@@ -61,6 +59,14 @@ class RB3Jay < Sinatra::Application
 			files_and_users.each{ |file,_| add file }
 		end
 		puts "%-20s: %.3fs" % ["add #{files_and_users.length} songs",(t=Time.now)-lap_time]; lap_time=t
+
+		unless files_and_users.empty?
+			begin
+				@mpd.song_priority(2,[initial_song_count..-1])
+			rescue MPD::ServerArgumentError
+			end
+			puts "%-20s: %.3fs" % ["set song priority",(t=Time.now)-lap_time]; lap_time=t
+		end
 
 		@db[:sticker]
 		.insert_conflict(:replace)
